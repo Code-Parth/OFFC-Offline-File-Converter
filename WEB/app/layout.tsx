b07e4ce80047798c654e205d65a16a41a638bdbc
Code-Parth <softsparths@gmail.com>
2024-02-11 06:32:10 +0530

    Add metadata to layout component

--- WEB/app/layout.tsx
+++ WEB/app/layout.tsx
@@ -2,6 +2,11 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Toaster } from '@/components/ui/toaster';
 
+export const metadata: Metadata = {
+    title: 'OffC - Offline Converter',
+    description: 'OffC is a free, open-source, offline file converter that supports 50+ formats.',
+}
+
 export default function RootLayout({
     children,
 }: {
